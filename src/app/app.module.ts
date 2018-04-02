0d7c29a659b63d8685664a6dfd097dcd4d7d1547
Debashish Sahu <debashishswp@gmail.com>
2018-04-02 09:19:16 +0530

    intial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputTextComponent } from './input-text/input-text.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, InputTextComponent
+    AppComponent, InputTextComponent, HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule
